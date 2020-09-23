4da080cb188201f2df94297a23c9b5bc1e0d80be
XFlasher <45360887+XFlasher@users.noreply.github.com>
2020-09-23 20:31:47 +0300

    3.0

--- Modules/Events.py
+++ Modules/Events.py
@@ -14,40 +14,18 @@ class Events(commands.Cog):
         if ctx.guild:
             await ctx.message.delete()
 
+    """
+    @commands.Cog.listener()
+    async def on_guild_update(self, before, after):
+        if before.icon_url != after.icon_url:
+            await self.Client.user.edit(avatar = after.icon_url)
+    """
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             return await ctx.send('**У вас недостаточно прав для выполнения этого.**', delete_after = 10)
         elif isinstance(error, commands.BadUnionArgument):
             return await ctx.send('**Вы ввели не верный или не подходящий аргумент.**', delete_after = 10)
-
-    @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        passid = (541172771200434188, 353690811931820034)
-        if after.id in passid:
-            return
-        pex = ' | Imp.❤'
-        guild_role = discord.utils.get(after.guild.roles, id = 709748813199310929)
-        if guild_role is None:
-            pass
-        else:
-            if before.nick != after.nick and guild_role in after.roles:
-                if after.nick != None and pex in after.nick:
-                    pass
-                else:
-                    nick = after.nick
-                    if nick is None:
-                        nick = after.name
-                    await after.edit(nick = f'{nick}{pex}')
-            if guild_role in after.roles and guild_role not in before.roles:
-                nick = after.nick
-                if nick == None:
-                    nick = after.name
-                await after.edit(nick = f'{nick + pex}')
-            elif guild_role not in after.roles and guild_role in before.roles:
-                await after.edit(nick = f'{after.name}')
-            else:
-                pass
 
 
     @commands.Cog.listener()
--- Modules/moderation.py
+++ Modules/moderation.py
@@ -187,28 +187,6 @@ class Moderation(commands.Cog):
         else:
             pass
 
-    @commands.command(hidden = False, description = 'Проверить префикс гильдии у пользователей.')
-    @commands.has_permissions(manage_roles = True)
-    async def guildcheck(self, ctx):
-        passid = (541172771200434188, 353690811931820034)
-        pex = ' | Imp.❤'
-        changed_members = 0
-        guild_role = discord.utils.get(ctx.guild.roles, id = 709748813199310929)
-        for member in ctx.guild.members:
-            if member.id in passid:
-                pass
-            else:
-                if guild_role in member.roles:
-                    nick = member.nick
-                    if nick == None:
-                        nick = member.name
-                    if pex in str(nick):
-                        pass
-                    else:
-                        changed_members += 1
-                        await member.edit(nick = f'{nick}{pex}')
-        await ctx.send(f'**Успешно отредактировано  {members_count(changed_members)}**', delete_after = 12)
-
 
 
 
